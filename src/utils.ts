0a90866c02dcd47509c9a506e4667f27d0d9e839
Soren Mathiasen <sorenm@mymessages.dk>
2023-09-13 11:38:17 +0200

    fix: fixing potetial null values

--- src/utils.ts
+++ src/utils.ts
@@ -6,12 +6,12 @@ export const PORT_TYPE = 'postgres';
 export async function createDBURI(provider:ConfigProvider, resourceName: string) {
     const dbInfo = await provider.getResourceInfo(RESOURCE_TYPE, PORT_TYPE, resourceName);
     const dbName =
-        dbInfo.options && dbInfo.options.dbName
+        dbInfo?.options && dbInfo.options.dbName
             ? dbInfo.options.dbName
             : resourceName;
 
     let credentials = ''
-    if (dbInfo.credentials?.username) {
+    if (dbInfo?.credentials?.username) {
         credentials += encodeURIComponent(dbInfo.credentials.username);
 
         if (dbInfo.credentials.password) {
@@ -19,5 +19,5 @@ export async function createDBURI(provider:ConfigProvider, resourceName: string)
         }
     }
 
-    return `postgresql://${credentials}@${dbInfo.host}:${dbInfo.port}/${encodeURIComponent(dbName)}`;
+    return `postgresql://${credentials}@${dbInfo?.host}:${dbInfo?.port}/${encodeURIComponent(dbName)}`;
 }
