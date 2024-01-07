04dd5afbbbf5ead287d42f77493403b12cede5b9
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-07 16:07:31 +0100

    feat: Add functional client creator and documentation (#9)

--- src/PostgresDB.ts
+++ src/PostgresDB.ts
@@ -11,12 +11,41 @@ interface PrismaClient {
     $disconnect(): Promise<void>;
 }
 
+/**
+ * Create a postgres client and connect it to the database
+ *
+ * @param config the kapeta config provider
+ * @param resourceName the resource name within the block
+ * @param createClient a function that creates the prisma client
+ */
+export const createPostgresDBClient = async <T extends PrismaClient>(config:ConfigProvider, resourceName: string, createClient: (opts: any) => T) => {
+    const url = await createDBURI(config, resourceName);
+    console.log('Connecting to postgres database: %s', resourceName);
 
+    const prisma = createClient({
+        datasources: {
+            db: {
+                url
+            },
+        },
+    });
+
+    await prisma.$connect();
+    console.log('Connected successfully to postgres database: %s', resourceName);
+    return prisma;
+}
+
+/**
+ * A base class for postgres databases.
+ *
+ * See also {@link createPostgresDBClient} which is the recommended way to create a postgres client.
+ */
 export abstract class PostgresDB<T extends PrismaClient> {
     private readonly _resourceName: string;
     private _ready: boolean = false;
     private _prisma?: T;
-    constructor(resourceName:string) {
+
+    protected constructor(resourceName:string) {
         this._resourceName = resourceName;
         Config.onReady(async (provider) => {
             await this.init(provider);
@@ -26,19 +55,7 @@ export abstract class PostgresDB<T extends PrismaClient> {
     abstract createClient(opts: any): T;
 
     async init(provider: ConfigProvider) {
-        const url = await createDBURI(provider, this._resourceName);
-        console.log('Connecting to postgres database: %s', url);
-
-        this._prisma = this.createClient({
-            datasources: {
-                db: {
-                    url
-                },
-            },
-        });
-
-        await this._prisma.$connect();
-        console.log('Connected successfully to postgres database: %s', url);
+        this._prisma = await createPostgresDBClient(provider, this._resourceName, this.createClient);
         this._ready = true;
     }
 
